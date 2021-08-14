b11ac81dfcb9ee9902115449290079c6514ee34f
ankitverma-123-code <av4135850.19je0144@cse.iitism.ac.in>
2021-08-14 06:21:27 +0530

    complete till list down of all elements using array map method

--- src/App.js
+++ src/App.js
@@ -7,21 +7,21 @@ const App = () => {
   const expenses = [
     {
       id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
+      title: 'Laptop',
+      amount: 55000,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e2', title: 'New TV', amount: 25000, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
-      amount: 294.67,
+      amount: 15000,
       date: new Date(2021, 2, 28),
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
-      amount: 450,
+      amount: 2000,
       date: new Date(2021, 5, 12),
     },
   ];
@@ -30,13 +30,6 @@ const App = () => {
     console.log('In App.js');
     console.log(expense);
   };
-
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
 
   return (
     <div>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,7 +16,14 @@ const Expenses = (props) => {
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
+        {props.items.map((expenses)=>
+            <ExpenseItem
+              title={expenses.title}
+              amount={expenses.amount}
+              date={expenses.date}
+            />
+        )}
+        {/* <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
@@ -35,7 +42,7 @@ const Expenses = (props) => {
           title={props.items[3].title}
           amount={props.items[3].amount}
           date={props.items[3].date}
-        />
+        /> */}
       </Card>
     </div>
   );
